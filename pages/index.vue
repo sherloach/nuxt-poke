65b93dce02e62449ce34a814dd265c43cafc7cc7
Hayden Ngo <hoango1607@gmail.com>
2023-12-01 23:02:30 +1030

    chore: init project

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const appConfig = useAppConfig()
+</script>
+
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert>
+        This is an auto-imported component
+    </AppAlert>
+  </div>
+</template>
