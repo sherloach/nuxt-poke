cc5e1433a289afcdfbf915558aa678f7efb29ef8
Hayden Ngo <hoango1607@gmail.com>
2023-12-03 13:17:52 +1030

    feat: add tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@ const { data: pokemons } = await useFetch("/api/pokemon?offset=1");
 
 <template>
   <div>
-    <h1>Welcome to the Pokedex</h1>
+    <h1 class="text-3xl">Welcome to the Pokedex</h1>
     <ul v-for="pokemon in pokemons">
       <li>{{ pokemon.name }} x {{ pokemon.url }}</li>
     </ul>
